5e40ca9f93887747aa6f66ef16eda541868e24b9
hhawk51 <silvers.austin.l@gmail.com>
2022-12-09 14:11:06 -0500

    Commit

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,39 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header />
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a>
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+
+          </a>
+
+
       </main>
 
       <Footer />
     </div>
+</div>
   )
-}
+} 
+
+
